3c6e3f02b0fc88d0f3575224cc23bed0677f30ea
JAntos <jantos@casenetllc.com>
2022-07-01 05:04:16 -0400

    Basic bitshift demo

--- src/Sea/Fish/OperatorCarp.java
+++ src/Sea/Fish/OperatorCarp.java
@@ -8,13 +8,36 @@ public class OperatorCarp {
         System.out.println("i++ "+ i++); //++ evaluated after printing
         System.out.println(i++ +" i++" ); //++ also evaluated after printing
         System.out.println("++i: "+ ++i); //evaluated before printing
+    }
 
+    public void modulusDemo() {
+        int i = 63, j = 5;
+        float f = 10.0f, g = 3.0f;
+
+        System.out.println( i +" Mod "+ j + " = "+ i%j);
+        System.out.println( f +" Mod "+ g + " = "+ f%g);
+        //System.out.println( i +" Mod "+ 0 + " = "+ f%0);//throws exception
+        System.out.println( f +" Mod "+ 0.0f + " = "+ f%0.0f);
+    }
+
+    public void shiftDemo() {
+        int i = Integer.MIN_VALUE;
+        ///unsigned bit shift
+        System.out.println(
+                String.format("%32s", Integer.toBinaryString(i >>> 1)).replace(' ','0')
+        );
+        ///signed bit shift
+        System.out.println(
+                String.format("%32s", Integer.toBinaryString(i >> 1)).replace(' ','0')
+        );
     }
 
 
     public static void main(String[] args) {
         OperatorCarp oc = new OperatorCarp();
 
-        oc.incrementDemo();
+        //oc.incrementDemo();
+        //oc.modulusDemo();
+        oc.shiftDemo();
     }
 }
